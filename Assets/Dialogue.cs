af086038d1025c6a5221208540e54244326ea923
Shuen Si <shawn_si@hotmail.com>
2021-05-07 20:04:11 -0400

    DialogueManager commit test

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Dialogue {
+public class Dialogue
+{
 
-	public string name;
+    public string name;
 
-	[TextArea(3, 10)]
-	public string[] sentences;
+    [TextArea(3, 10)]
+    public string[] sentences;
 
 }
--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -3,62 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DialogueManager : MonoBehaviour {
+public class DialogueManager : MonoBehaviour
+{
 
-	public Text nameText;
-	public Text dialogueText;
+    public Text nameText;
+    public Text dialogueText;
 
-	public Animator animator;
+    public Animator animator;
 
-	private Queue<string> sentences;
+    private Queue<string> sentences;
 
-	// Use this for initialization
-	void Start () {
-		sentences = new Queue<string>();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        sentences = new Queue<string>();
+        Debug.Log("Git push test");
+    }
 
-	public void StartDialogue (Dialogue dialogue)
-	{
-		animator.SetBool("IsOpen", true);
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
 
-		nameText.text = dialogue.name;
+        nameText.text = dialogue.name;
 
-		sentences.Clear();
+        sentences.Clear();
 
-		foreach (string sentence in dialogue.sentences)
-		{
-			sentences.Enqueue(sentence);
-		}
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
 
-		DisplayNextSentence();
-	}
+        DisplayNextSentence();
+    }
 
-	public void DisplayNextSentence ()
-	{
-		if (sentences.Count == 0)
-		{
-			EndDialogue();
-			return;
-		}
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
 
-		string sentence = sentences.Dequeue();
-		StopAllCoroutines();
-		StartCoroutine(TypeSentence(sentence));
-	}
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
 
-	IEnumerator TypeSentence (string sentence)
-	{
-		dialogueText.text = "";
-		foreach (char letter in sentence.ToCharArray())
-		{
-			dialogueText.text += letter;
-			yield return null;
-		}
-	}
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
 
-	void EndDialogue()
-	{
-		animator.SetBool("IsOpen", false);
-	}
+    void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+    }
 
 }
